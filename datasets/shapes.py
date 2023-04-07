00a6fc4b0e773df3bad4beb667c76a381e7266ca
Raghav Singhal <63965100+RaghavSinghal10@users.noreply.github.com>
2023-04-07 16:20:59 +0530

    Fixed triangle vanishing issue at low p

--- datasets/shapes.py
+++ datasets/shapes.py
@@ -42,16 +42,18 @@ def triangle_improved(r, p, device, thickness=3):
 
     if r < 100:
       thickness = 2
+
     mid_1 = r // 2
     outer_1 = torch.distributions.Bernoulli(probs=p).sample((r, r)).to(device).bool()
+    outer_full_1 = torch.distributions.Bernoulli(probs=1).sample((r, r)).to(device).bool()
 
     # Outer_1
     outer_left_1 = outer_1[:mid_1, :mid_1].tril(0).flip(1).repeat_interleave(2, 1)
 
-    outer_1[:mid_1] &= outer_left_1
-    outer_1[mid_1:] &= outer_left_1.flip(0)
+    outer_full_1[:mid_1] &= outer_left_1
+    outer_full_1[mid_1:] &= outer_left_1.flip(0)
 
-    # Inner
+    #Inner
     inner_left = (
         torch.ones(mid_1 - thickness, mid_1 - thickness, device=device)
         .tril(2)
@@ -59,21 +61,22 @@ def triangle_improved(r, p, device, thickness=3):
         .flip(0)
         .repeat_interleave(2, 1)
     )
-    outer_1[thickness:mid_1, thickness : r - thickness] &= inner_left
-    outer_1[mid_1:-thickness, thickness : r - thickness] &= inner_left.flip(0)
+    outer_full_1[thickness:mid_1, thickness : r - thickness] &= inner_left
+    outer_full_1[mid_1:-thickness, thickness : r - thickness] &= inner_left.flip(0)
 
     scale = 0.75 + r/2000
     r_2 = int(round(scale*r/2) * 2)
 
     mid_2 = r_2 // 2
     outer_2 = torch.distributions.Bernoulli(probs=p).sample((r_2, r_2)).to(device).bool()
+    outer_full_2 = torch.distributions.Bernoulli(probs=1).sample((r_2, r_2)).to(device).bool()
 
     # Outer_2
     outer_left_2 = outer_2[:mid_2, :mid_2].tril(0).flip(1).repeat_interleave(2, 1)
 
 
-    outer_2[:mid_2] &= outer_left_2
-    outer_2[mid_2:] &= outer_left_2.flip(0)
+    outer_full_2[:mid_2] &= outer_left_2
+    outer_full_2[mid_2:] &= outer_left_2.flip(0)
 
     # Inner
     inner_left = (
@@ -83,8 +86,8 @@ def triangle_improved(r, p, device, thickness=3):
         .flip(0)
         .repeat_interleave(2, 1)
     )
-    outer_2[thickness:mid_2, thickness : r_2 - thickness] &= inner_left
-    outer_2[mid_2:-thickness, thickness : r_2 - thickness] &= inner_left.flip(0)
+    outer_full_2[thickness:mid_2, thickness : r_2 - thickness] &= inner_left
+    outer_full_2[mid_2:-thickness, thickness : r_2 - thickness] &= inner_left.flip(0)
 
     r_1 = r
     size_padded = int(round(((r - r_2)/2)))
@@ -96,9 +99,9 @@ def triangle_improved(r, p, device, thickness=3):
       size_padded_1 = int((r_1 - r_2)/2)
       size_padded_2 = int((r_1 - r_2 + 1)/2)
 
-    outer_2_padded = torch.nn.functional.pad(outer_2, (int(round(1.5*size_padded)), int(round(0.5*size_padded)), size_padded_1, size_padded_2), "constant", 0)
+    outer_2_padded = torch.nn.functional.pad(outer_full_2, (int(round(1.5*size_padded)), int(round(0.5*size_padded)), size_padded_1, size_padded_2), "constant", 0)
 
-    outer = outer_1 + outer_2_padded
+    outer = outer_full_1 + outer_2_padded
 
     return outer
 
